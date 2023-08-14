7ba81ac4b13f7ad148faa054dab39ceaaf56f9ed
Joel Joboy <joeljoboyjoseph75@gmail.com>
2023-08-14 20:17:34 +0530

    Working on js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var timer=60;
+var score=0;
+function scoreIncrease()
+{
+    score += 10;
+    document.querySelector("#scoreVal").textContent=score;
+}
+function getNewHit()
+{
+    var rn=Math.floor(Math.random()*10);
+    document.querySelector("#hitVal").textContent=rn;
+}
+function Bubbler()
+{
 var clutter="";
 for(var i=1;i<=102;i++)
 {
@@ -5,3 +19,21 @@ for(var i=1;i<=102;i++)
     clutter+=`<div class="bubble">${random}</div>`
 }
 document.querySelector("#pbottom").innerHTML=clutter;
+}
+function runTimer()
+{
+     var timerrun=setInterval(function(){
+        if (timer>0) {
+            timer--;
+            document.querySelector("#timerValue").textContent=timer;
+        }
+        else
+        {
+            clearInterval(timerrun)
+        }
+    },1000);
+}
+Bubbler();
+runTimer();
+getNewHit();
+scoreIncrease();
